de0abb8bce5636259f5cb2973e86bdeeafb60ec5
Ayomide24500 <Ayomideadisa83@gmail.com>
2024-07-26 22:10:48 +0100

    verify

--- util/email.ts
+++ util/email.ts
@@ -3,12 +3,14 @@ import nodemailer from "nodemailer";
 import path from "path";
 import ejs from "ejs";
 import jwt from "jsonwebtoken";
-const GOOGLE_ID =
-  "338600539682-3p0gjosdmnrt4r6tnj5ubf5hjvnugfi7.apps.googleusercontent.com";
-const GOOGLE_SECRET = "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";
-const GOOGLE_REDIRECT_URL = "https://developers.google.com/oauthplayground";
-const GOOGLE_REFRESH =
-  "1//04hwuB3riCsPbCgYIARAAGAQSNwF-L9IrpC2o9EZUtAuvSsDpLN7h6o0eTzVk61JuQZq0-TdlYhI4sHtrVfAbM2-m1FWFirU8Qww";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const GOOGLE_ID = process.env.GOOGLE_ID!;
+const GOOGLE_SECRET = process.env.GOOGLE_SECRET!;
+const GOOGLE_REDIRECT_URL = process.env.GOOGLE_REDIRECT_URL!;
+const GOOGLE_REFRESH = process.env.GOOGLE_REFRESH!;
 
 const oAuth = new google.auth.OAuth2(
   GOOGLE_ID,
@@ -76,42 +78,42 @@ export const sendEmail = async (admin: any) => {
   }
 };
 
-export const sendResetPasswordEmail = async (user: any) => {
-  try {
-    const accessToken: any = (await oAuth.getAccessToken()).token;
+// export const sendResetPasswordEmail = async (user: any) => {
+//   try {
+//     const accessToken: any = (await oAuth.getAccessToken()).token;
 
-    const transport = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: "ayomideadisa83@gmail.com",
-        clientSecret: GOOGLE_SECRET,
-        clientId: GOOGLE_ID,
-        refreshToken: GOOGLE_REFRESH,
-        accessToken,
-      },
-    });
-    const getFile = path.join(__dirname, "../views/resetPassword.ejs");
+//     const transport = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         type: "OAuth2",
+//         user: "ayomideadisa83@gmail.com",
+//         clientSecret: GOOGLE_SECRET,
+//         clientId: GOOGLE_ID,
+//         refreshToken: GOOGLE_REFRESH,
+//         accessToken,
+//       },
+//     });
+//     const getFile = path.join(__dirname, "../views/resetPassword.ejs");
 
-    const data = {
-      token: user.token,
-      email: user.email,
-      url: `${URL}/user-verify/${user._id}`,
-    };
+//     const data = {
+//       token: user.token,
+//       email: user.email,
+//       url: `${URL}/user-verify/${user._id}`,
+//     };
 
-    const html = await ejs.renderFile(getFile, { data });
+//     const html = await ejs.renderFile(getFile, { data });
 
-    const mailer = {
-      from: "Ayomide 🚀👍 <ayomideadisa83@gmail.com>",
-      to: user.email,
-      subject: "Account Opening",
-      html,
-    };
+//     const mailer = {
+//       from: "Ayomide 🚀👍 <ayomideadisa83@gmail.com>",
+//       to: user.email,
+//       subject: "Account Opening",
+//       html,
+//     };
 
-    await transport.sendMail(mailer).then(() => {
-      console.log("send reset");
-    });
-  } catch (error) {
-    return error;
-  }
-};
+//     await transport.sendMail(mailer).then(() => {
+//       console.log("send reset");
+//     });
+//   } catch (error) {
+//     return error;
+//   }
+// };
