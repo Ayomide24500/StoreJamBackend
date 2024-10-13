347839443202d130ed8912f3171e5006cca5de0d
Ayomide24500 <142061740+Ayomide24500@users.noreply.github.com>
2024-10-13 19:20:53 +0100

    Update AdminController.ts

--- controller/AdminController.ts
+++ controller/AdminController.ts
@@ -7,11 +7,10 @@ import jwt from "jsonwebtoken";
 
 export const Register = async (req: any, res: Response) => {
   try {
-    const { firstName, lastName, email, verify, password } = req.body;
+    const { firstName, lastName, email, verify } = req.body;
     const id = crypto.randomBytes(4).toString("hex");
     const create = await AdminModel.create({
       firstName,
-      password,
       lastName,
       status: "Admin",
       email,
