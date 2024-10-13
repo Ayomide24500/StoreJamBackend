d311efa8e3f4efe002b961bcfe08037def5b14b8
Ayomide24500 <142061740+Ayomide24500@users.noreply.github.com>
2024-10-13 19:58:13 +0100

    Update AdminModel.ts

--- model/AdminModel.ts
+++ model/AdminModel.ts
@@ -8,7 +8,6 @@ interface IAdmin {
   token: string;
   verify: boolean;
   status: string;
-  password: string;
 }
 
 interface IAdminDocument extends IAdmin, Document {}
@@ -19,17 +18,7 @@ const AdminModel = new Schema<IAdminDocument>({
   email: { type: String, required: true, unique: true },
   token: { type: String },
   status: { type: String, required: true, default: "Admin" },
-  password: { type: String, required: true },
   verify: { type: Boolean, default: false },
-});
-
-AdminModel.pre("save", async function (next) {
-  const admin = this as IAdminDocument;
-  if (!admin.isModified("password")) return next();
-
-  const salt = await bcrypt.genSalt(10);
-  admin.password = await bcrypt.hash(admin.password, salt);
-  next();
 });
 
 export default model<IAdminDocument>("Admin", AdminModel);
